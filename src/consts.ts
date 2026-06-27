055a2060d7864b9f2438f6d8c7d965fd7168fada
nanytoto90-boop <nanytoto90@gmail.com>
2026-06-28 00:59:10 +0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'نشتري العفش المستعمل بالمنصورة';
+export const SITE_DESCRIPTION = 'نشتري الأثاث المستعمل وعفش الشقق والمنازل بأعلى الأسعار في المنصورة وضواحيها، خدمات فك ونقل فوري وكاش.';
